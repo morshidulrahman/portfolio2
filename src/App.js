6be463eb1c18492c87f07c412665d4b03c15dfba
morshidulrahman <boydanger416@gmail.com>
2022-10-22 19:12:00 +0600

    about

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import Navbar from './components/Navbar/Navbar';
+import Header from './container/Header/Header';
+import "./App.scss"
 const App = () => (
   <div>
     <Navbar />
+    <Header />
   </div>
 );
 
