f2c09eb9505f7ca1577c138d990b57554e108eed
morshidulrahman <boydanger416@gmail.com>
2022-10-24 00:06:30 +0600

    works

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import Navbar from './components/Navbar/Navbar';
 import Header from './container/Header/Header';
 import About from "./container/About/About"
+import Work from './container/Work/Work';
 import "./App.scss"
 const App = () => (
   <div>
     <Navbar />
     <Header />
     <About />
+    <Work />
   </div>
 );
 
---
+++ src/Data/index.js
@@ -0,0 +1,9 @@
+
+const Data = ['UI/UX', 'Web App', 'Mobile App', 'React JS', 'All']
+
+const AllDatas = [
+    { type: "all", description: "i am good developer", url: images1, projectLink: "http://facebok.com" },
+    { type: "all", description: "i am good developer", url: images1, projectLink: "http://facebok.com" },
+    { type: "React JS", description: "i am good react developer", url: images1, projectLink: "http://facebok.com" },
+    { type: "all", description: "i am good developer", url: images1, projectLink: "http://facebok.com" },
+]
