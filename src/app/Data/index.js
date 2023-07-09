3a11775749724790ef217d51dcd8eac1689fa53d
Morshidul Rahman <boydanger416@gmail.com>
2023-07-09 14:15:51 +0600

    Update index.js

--- src/app/Data/index.js
+++ src/app/Data/index.js
@@ -2,29 +2,104 @@
 export const Data = ['All', 'UI/UX', 'Mobile App', 'React JS']
 
 export const WORKSDATA = [
-    { type: "React JS", description: "Furniture website clone using react js firebase tailwind", url: "/assets/parabot.jpg", projectLink: "https://enchanting-taffy-306f45.netlify.app/" },
+  {
+    type: "React JS",
+    description: "a simple eccomerce mern photography website",
+    url: "/assets/rsz_photography_8.png",
+    projectLink: "https://photgrapy-morshdiul.netlify.app/",
+    gitlink: "https://github.com/morshidulrahman/photography-client",
+  },
+  {
+    type: "React JS",
+    description: "multimart ecommerce website clone using react firebase ",
+    url: "/assets/multimart.png",
+    projectLink: "https://multi-mart-ecommerce.netlify.app/",
+    gitlink: "https://github.com/morshidulrahman/multiecoomerce",
+  },
+  {
+    type: "UI/UX",
+    description: "Little netlify clone using react tailwind rest api",
+    url: "/assets/netlify.jpg",
+    projectLink: "https://lucky-zuccutto-459e25.netlify.app/",
+  },
+  {
+    type: "React JS",
+    description: "a simple eccomerce mern toymart website",
+    url: "/assets/rsz_toy-home.jpg",
+    projectLink: "https://github.com/morshidulrahman/toymart-client",
+  },
+  {
+    type: "React JS",
+    description: "Delivery website clone using react tailwind firebase",
+    url: "/assets/delivery.jpg",
+    projectLink: "https://comforting-valkyrie-56db76.netlify.app/",
+  },
+  {
+    type: "React JS",
+    description: "Planet website clone using javascript and css",
+    url: "/assets/planet.jpg",
+    projectLink: "https://planet2.netlify.app/",
+    gitlink: "https://github.com/morshidulrahman/planet",
+  },
+  {
+    type: "React JS",
+    description: "Furniture website clone using react js firebase tailwind",
+    url: "/assets/parabot.jpg",
+    projectLink: "https://enchanting-taffy-306f45.netlify.app/",
+  },
+  {
+    type: "React JS",
+    description: "Unplash website clone using javascript html css",
+    url: "/assets/unplash.jpg",
+    projectLink: "https://unplashapi.netlify.app/index.html",
+  },
 
-    { type: "React JS", description: "Unplash website clone using javascript html css", url: "/assets/unplash.jpg", projectLink: "https://unplashapi.netlify.app/index.html" },
+  {
+    type: "React JS",
+    description: "Bootloader website clone using bootstarp",
+    url: "/assets/bootloader.jpg",
+    projectLink:
+      "https://gracious-meitner-40452e.netlify.app/?fbclid=IwAR0wQqt1exAEtzpdBzV93P4854rBAraYibsc2bwsulVhIIr8-q7JkmTkQiw#",
+  },
 
-    { type: "React JS", description: "Delivery website clone using react tailwind firebase", url: "/assets/delivery.jpg", projectLink: "https://comforting-valkyrie-56db76.netlify.app/" },
+  {
+    type: "React JS",
+    description: "Portfolio using react and tailwind-css",
+    url: "/assets/portfolio.jpg",
+    projectLink: "https://morshidulrahman.netlify.app/",
+  },
 
-    { type: "React JS", description: "Planet website clone using javascript and css", url: "/assets/planet.jpg", projectLink: "https://planet2.netlify.app/", gitlink: "https://github.com/morshidulrahman/planet" },
+  {
+    type: "React JS",
+    description: "zeptoclone using html css javascript",
+    url: "/assets/zepto.jpg",
+    projectLink: "https://helpful-pony-3fd223.netlify.app/",
+  },
 
-    { type: "UI/UX", description: "Little netlify clone using react tailwind rest api", url: "/assets/netlify.jpg", projectLink: "https://lucky-zuccutto-459e25.netlify.app/" },
+  {
+    type: "Web App",
+    description: "Megakit clone using bootstrap ",
+    url: "/assets/megakit.jpg",
+    projectLink: "https://morshidulrahman.github.io/megakit-website/",
+    gitlink: "https://github.com/morshidulrahman/megakit-website",
+  },
 
-    { type: "React JS", description: "Bootloader website clone using bootstarp", url: "/assets/bootloader.jpg", projectLink: "https://gracious-meitner-40452e.netlify.app/?fbclid=IwAR0wQqt1exAEtzpdBzV93P4854rBAraYibsc2bwsulVhIIr8-q7JkmTkQiw#" },
+  {
+    type: "Mobile App",
+    description:
+      "Delivaro 2.0 using React-native-expo tailwind css firebase google map",
+    url: "/assets/phone.jpg",
+    projectLink: "#",
+    gitlink: "https://github.com/morshidulrahman/delivaro",
+  },
 
-    { type: "React JS", description: "Portfolio using react and tailwind-css", url: "/assets/portfolio.jpg", projectLink: "https://morshidulrahman.netlify.app/" },
-
-    { type: "React JS", description: "zeptoclone using html css javascript", url: "/assets/zepto.jpg", projectLink: "https://helpful-pony-3fd223.netlify.app/" },
-
-    { type: "Web App", description: "Megakit clone using bootstrap ", url: "/assets/megakit.jpg", projectLink: "https://morshidulrahman.github.io/megakit-website/", gitlink: "https://github.com/morshidulrahman/megakit-website" },
-
-    { type: "Mobile App", description: "Delivaro 2.0 using React-native-expo tailwind css firebase google map", url: "/assets/phone.jpg", projectLink: "#" },
-
-    { type: "Mobile App", description: "Music app clone using javascript css api ", url: "/assets/musicapp.jpg", projectLink: "https://tranquil-capybara-e5f114.netlify.app/" },
-
-]
+  {
+    type: "Mobile App",
+    description: "Music app clone using javascript css api ",
+    url: "/assets/musicapp.jpg",
+    projectLink: "https://tranquil-capybara-e5f114.netlify.app/",
+  },
+];
 
 export const HeroImage = [{ URL: "/assets/react.png" }, { URL: "/assets/redux.png" }, { URL: "/assets/sass.png" },]
 
